21322b712ccecb9eccdafdee7a1192e46f552bbe
Leonardo Mendes <leonardomtads@gmail.com>
2021-06-25 20:08:47 -0300

    chore: add config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,1 +1,11 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+
+const rootImport = [
+  'root-import',
+  {
+    rootPathPrefix: '@',
+    rootPathSuffix: 'src',
+  },
+];
+
+module.exports = (config) => injectBabelPlugin(rootImport, config);
