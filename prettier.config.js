1c52603dc7935660deb9f02e5f6d15f420f3a1af
Leonardo Mendes <leonardomtads@gmail.com>
2021-06-24 22:52:35 -0300

    chore: prettier config

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always', // Always include parens. Example: (x) => x
+	//arrowParens: 'avoid', // Omit parens when possible. Example: x => x
+}
