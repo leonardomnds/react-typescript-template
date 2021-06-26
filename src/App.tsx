9506570150d889b7da60849c54155b19d3d1d537
Leonardo Mendes <leonardomtads@gmail.com>
2021-06-25 22:15:18 -0300

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyles from './styles/global';
 import { Routes } from './routes';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyles from './styles/global';
 import App from './App';
 
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>,
   document.getElementById('root'),
