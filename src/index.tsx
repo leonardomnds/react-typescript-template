4bbd476f49644f50e4185caeeb642d9e8121e95b
Leonardo Mendes <leonardomtads@gmail.com>
2021-06-26 13:06:17 -0300

    chore: remove styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyles from './styles/global';
 import App from './App';
 
 ReactDOM.render(
   <StrictMode>
-    <GlobalStyles />
     <App />
   </StrictMode>,
   document.getElementById('root'),
