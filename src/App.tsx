5add30105d602764a1d7f8dbdf942bc30acc97ad
Leonardo Mendes <leonardomtads@gmail.com>
2021-06-25 21:24:18 -0300

    chore: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
 import { Routes } from './routes';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
